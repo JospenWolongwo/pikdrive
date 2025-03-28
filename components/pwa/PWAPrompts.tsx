d20ad1640abd68f6ac8ec418dc9a20c7be54c6ac
Jospen Wolongwo <jospenwolongwo@gmail.com>
2025-03-28 13:59:57 +0100

    fix: build

--- components/pwa/PWAPrompts.tsx
+++ components/pwa/PWAPrompts.tsx
@@ -1,13 +1,28 @@
 'use client';
 
+import { useState } from 'react';
 import { InstallPrompt } from './InstallPrompt';
 import { IOSInstallPrompt } from './IOSInstallPrompt';
+import { AndroidInstallPrompt } from './AndroidInstallPrompt';
 
 export function PWAPrompts() {
+  const [showIOS, setShowIOS] = useState(false);
+  const [showAndroid, setShowAndroid] = useState(false);
+
   return (
     <>
-      <InstallPrompt />
-      <IOSInstallPrompt />
+      <IOSInstallPrompt 
+        show={showIOS}
+        onDismiss={() => setShowIOS(false)}
+      />
+      <AndroidInstallPrompt 
+        show={showAndroid}
+        onDismiss={() => setShowAndroid(false)}
+        onInstall={() => {
+          console.log('🤖 Installing from PWAPrompts');
+          setShowAndroid(false);
+        }}
+      />
     </>
   );
 }
